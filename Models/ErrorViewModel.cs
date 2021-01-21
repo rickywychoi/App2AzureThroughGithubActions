754d3127566570ff7175928cccc5c84117834e54
rickywychoi <cchaff0105@gmail.com>
2021-01-21 11:26:30 -0800

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace App2AzureThroughGithubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
